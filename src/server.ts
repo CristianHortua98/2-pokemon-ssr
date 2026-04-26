4e838e53c33fd539bbc94e9efe9f5dcfa08bdebd
CAS <alejo.hortua3@gmail.com>
2026-04-25 21:58:52 -0500

    add netlfy/angular-runtime

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,17 @@ import {
 } from '@angular/ssr/node';
 import express from 'express';
 import { join } from 'node:path';
+import { CommonEngine } from "@angular/ssr/node";
+import { render } from "@netlify/angular-runtime/common-engine";
+ 
+const commonEngine = new CommonEngine();
+ 
+export async function netlifyCommonEngineHandler(
+  request: Request,
+  context: any
+): Promise<Response> {
+  return await render(commonEngine);
+}
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
