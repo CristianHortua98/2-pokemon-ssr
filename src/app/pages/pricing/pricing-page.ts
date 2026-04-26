00da235e51e11e21317b35e65cd62da38c57b3bf
CAS <alejo.hortua3@gmail.com>
2026-04-25 21:50:42 -0500

    Pokemon SRR

---
+++ src/app/pages/pricing/pricing-page.ts
@@ -0,0 +1,29 @@
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'page-pricing',
+  imports: [],
+  templateUrl: './pricing-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricingPage implements OnInit{
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+
+    // if(isPlatformBrowser(this.platform)){
+    //   document.title = 'Pricing Page';
+    // }
+
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({name: 'description', content: 'Este es mi Pricing Page'});
+    this.meta.updateTag({name: 'og:title', content: 'Pricing Page'});
+    this.meta.updateTag({name: 'keywords', content: 'Hola,Mundo,Cristian,Hortua,Angular,PRO'});
+  }
+
+}
